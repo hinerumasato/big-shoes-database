cf4e86e7beb9cb714b6f08f9ed0ecb1cfd156230
Thang Loi Tran <thangloitran406@gmail.com>
2022-12-14 19:04:41 +0700

    Update

---
+++ Scripts/index.js
@@ -0,0 +1,24 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now()
+    }
+    // Continue to JSON Server router
+    next()
+})
+
+server.use(router)
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log('JSON Server is running')
+})
